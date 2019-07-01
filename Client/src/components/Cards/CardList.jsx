61f564f4273596315336cf31ed313c4090bf002f
Sean Norwood <norwood.sean@gmail.com>
2019-07-01 12:10:21 -0500

    fixes(cardlist): fixes cards refreshing on theme change

--- Client/src/components/Cards/CardList.jsx
+++ Client/src/components/Cards/CardList.jsx
@@ -7,30 +7,23 @@ import { useStoreState, useStoreActions } from 'easy-peasy'
 import _ from 'lodash'
 
 const CardList = () => {
-  const isDark = useStoreState(state => state.theme.isDark)
   const zones = useStoreState(state => state.zones.zoneList)
   const getZones = useStoreActions(actions => actions.zones.getZones)
   const loadingZones = useStoreState(state => state.zones.loadingZones)
   const fourZones = _.chunk(zones, 4)
   const randomZones = fourZones[Math.floor(Math.random() * fourZones.length)]
 
-  // useEffect(() => {
-  //   if (zones.length < 1) {
-  //     getZones()
-  //   }
-  // }, [])
-
+  useEffect(() => {
+    if (zones.length < 1) {
+      getZones()
+    }
+  }, [])
   if (loadingZones) {
     return (
       <section className="section">
         <div className="columns">
           <div className="column is-three-fifths is-offset-two-fifths">
-            <ReactLoading
-              color={isDark ? '#ffffff' : '#292930'}
-              type={'bars'}
-              height={100}
-              width={100}
-            />
+            <ReactLoading type={'bars'} height={100} width={100} />
           </div>
         </div>
       </section>
@@ -61,5 +54,8 @@ const CardList = () => {
     )
   }
 }
-
+CardList.whyDidYouRender = {
+  logOnDifferentValues: true,
+  customName: 'EnhancedMenu'
+}
 export default CardList
--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App.jsx'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV !== 'production') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js')
+  whyDidYouRender(React)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
